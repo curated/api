539e00c0e06e4c4a3b2756060a3f4dbf4f908903
Wagner Camarao <wagner@camarao.io>
2018-06-16 14:47:04 +1000

    Use glog in http server

--- server/server.go
+++ server/server.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 
+	"github.com/golang/glog"
+
 	"github.com/curated/elastic/config"
-	"github.com/curated/elastic/logger"
 	"github.com/labstack/echo"
 	"github.com/olivere/elastic"
 )
@@ -33,42 +32,12 @@ var issueSortOptions = map[string]bool{
 	"updatedAt":      true,
 }
 
-// New creates the server
-func New() *Server {
-	cfg := config.Load()
-	lg := logger.New()
-
-	cli, err := elastic.NewClient(
-		elastic.SetURL(cfg.Elastic.URL),
-		elastic.SetBasicAuth(cfg.Elastic.Username, cfg.Elastic.Password),
-		elastic.SetScheme("https"),
-		elastic.SetSniff(false),
-	)
-
-	if err != nil {
-		lg.Printf("Failed creating server: %v", err)
-		os.Exit(1)
-	}
-
-	s := &Server{
-		Context: context.Background(),
-		Client:  cli,
-		Echo:    echo.New(),
-		Config:  cfg,
-		Logger:  lg,
-	}
-
-	s.Echo.GET("/issues", s.searchIssues)
-	return s
-}
-
 // Server serves HTTP requests
 type Server struct {
 	Context context.Context
 	Client  *elastic.Client
 	Echo    *echo.Echo
 	Config  *config.Config
-	Logger  *log.Logger
 }
 
 // IssuesRequest structure
@@ -84,9 +53,35 @@ type IssuesResponse struct {
 	Issues []interface{} `json:"issues"`
 }
 
+// New creates the server
+func New() *Server {
+	cfg := config.New()
+
+	cli, err := elastic.NewClient(
+		elastic.SetURL(cfg.Elastic.URL),
+		elastic.SetBasicAuth(cfg.Elastic.Username, cfg.Elastic.Password),
+		elastic.SetScheme("https"),
+		elastic.SetSniff(false),
+	)
+
+	if err != nil {
+		glog.Fatalf("Failed creating server: %v", err)
+	}
+
+	s := &Server{
+		Context: context.Background(),
+		Client:  cli,
+		Echo:    echo.New(),
+		Config:  cfg,
+	}
+
+	s.Echo.GET("/issues", s.searchIssues)
+	return s
+}
+
 // Start initializes the server
 func (s *Server) Start() {
-	s.Logger.Fatal(s.Echo.Start(":1323"))
+	glog.Fatal(s.Echo.Start(":1323"))
 }
 
 func (s *Server) searchIssues(c echo.Context) error {
@@ -104,7 +99,7 @@ func (s *Server) searchIssues(c echo.Context) error {
 		Do(s.Context)
 
 	if err != nil {
-		s.Logger.Printf("Failed searching issues: %v", err)
+		glog.Errorf("Failed searching issues: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
@@ -115,10 +110,12 @@ func (s *Server) searchIssues(c echo.Context) error {
 	for _, hit := range sr.Hits.Hits {
 		var issue map[string]interface{}
 		err := json.Unmarshal(*hit.Source, &issue)
+
 		if err != nil {
-			s.Logger.Printf("Failed parsing issue: %v\n%s", err, string(*hit.Source))
+			glog.Errorf("Failed parsing issue: %v\n%s", err, string(*hit.Source))
 			return c.NoContent(http.StatusInternalServerError)
 		}
+
 		res.Issues = append(res.Issues, issue)
 	}
 
